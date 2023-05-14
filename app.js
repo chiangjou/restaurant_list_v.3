1298a68df59ed4fe13751982a7c0620ea84c6652
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-14 16:34:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 // require package used in the project
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const port = 3000
+const app = express()
+
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongo error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
