d93fd92cd644eee7ecb0590c520455e6d416e7a1
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-07 21:32:09 +0800

    feat: add user model

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,24 +1,15 @@
-// 載入 mongoose
 const mongoose = require('mongoose')
 
-// 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-// 設定連線到 mongoose (MONGODB_URL 設定在 .env 檔案)
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
 
-// 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線失敗
 db.on('error', () => {
   console.log('MongoDB error!')
 })
-// 連線成功
 db.once('open', () => {
   console.log('MongoDB connected!')
 })
